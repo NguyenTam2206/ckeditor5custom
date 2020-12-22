fb816794d78c82b8bd56d27527c0314a9f92e59b
Tran Ha Nam <tranhanam1999hn@gmail.com>
2020-12-22 12:58:46 +0700

    Change div to img

--- src/_utils/simplebtnediting.js
+++ src/_utils/simplebtnediting.js
@@ -34,21 +34,21 @@ export default class SimpleBtnEditing extends Plugin {
         conversion.for('upcast').elementToElement( {
             model: 'simpleBtn',
             view: {
-                name: 'div',
+                name: 'img',
                 classes: 'my-custom-box'
             }
         } )
         conversion.for('dataDowncast').elementToElement( {
             model: 'simpleBtn',
             view: {
-                name: 'div',
+                name: 'img',
                 classes: 'my-custom-box'
             }
         } )
         conversion.for( 'editingDowncast' ).elementToElement( {
             model: 'simpleBtn',
             view: ( modelElement, { writer: viewWriter } ) => {
-                const section = viewWriter.createEditableElement( 'div', { class: 'my-custom-box' } );
+                const section = viewWriter.createEditableElement( 'img', { class: 'my-custom-box' } );
 
                 return toWidgetEditable( section, viewWriter, { label: 'simple btn widget' } );
             }
