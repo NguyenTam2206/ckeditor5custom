922658a147fba470c66bcb2d8160a67e318cbe0b
nguyentam2206 <nguyenthientam.2206@gmail.com>
2021-09-27 11:51:35 +0700

    optimize css and change config font-size

--- src/ckeditor5-font/src/fontsize/fontsizeediting.js
+++ src/ckeditor5-font/src/fontsize/fontsizeediting.js
@@ -7,24 +7,24 @@
  * @module font/fontsize/fontsizeediting
  */
 
-import { Plugin } from 'ckeditor5/src/core';
-import { CKEditorError } from 'ckeditor5/src/utils';
-import { isLength, isPercentage } from 'ckeditor5/src/engine';
+import { Plugin } from "ckeditor5/src/core";
+import { CKEditorError } from "ckeditor5/src/utils";
+import { isLength, isPercentage } from "ckeditor5/src/engine";
 
-import FontSizeCommand from './fontsizecommand';
-import { normalizeOptions } from './utils';
-import { buildDefinition, FONT_SIZE } from '../utils';
+import FontSizeCommand from "./fontsizecommand";
+import { normalizeOptions } from "./utils";
+import { buildDefinition, FONT_SIZE } from "../utils";
 
 // Mapping of `<font size="..">` styling to CSS's `font-size` values.
 const styleFontSize = [
-	'x-small', // Size "0" equal to "1".
-	'x-small',
-	'small',
-	'medium',
-	'large',
-	'x-large',
-	'xx-large',
-	'xxx-large'
+  "x-small", // Size "0" equal to "1".
+  "x-small",
+  "small",
+  "medium",
+  "large",
+  "x-large",
+  "xx-large",
+  "xxx-large"
 ];
 
 /**
@@ -41,157 +41,155 @@ const styleFontSize = [
  * @extends module:core/plugin~Plugin
  */
 export default class FontSizeEditing extends Plugin {
-	/**
-	 * @inheritDoc
-	 */
-	static get pluginName() {
-		return 'FontSizeEditing';
-	}
+  /**
+   * @inheritDoc
+   */
+  static get pluginName() {
+    return "FontSizeEditing";
+  }
 
-	/**
-	 * @inheritDoc
-	 */
-	constructor( editor ) {
-		super( editor );
+  /**
+   * @inheritDoc
+   */
+  constructor(editor) {
+    super(editor);
 
-		// Define default configuration using named presets.
-		editor.config.define( FONT_SIZE, {
-			options: [
-				'tiny',
-				'small',
-				'default',
-				'big',
-				'huge'
-			],
-			supportAllValues: false
-		} );
-	}
+    // Define default configuration using named presets.
+    editor.config.define(FONT_SIZE, {
+      options: ["default", 8, 9, 10, 12, 14, 16, 18, 24, 30, 36, 48, 60],
+      supportAllValues: true
+    });
+  }
 
-	/**
-	 * @inheritDoc
-	 */
-	init() {
-		const editor = this.editor;
+  /**
+   * @inheritDoc
+   */
+  init() {
+    const editor = this.editor;
 
-		// Allow fontSize attribute on text nodes.
-		editor.model.schema.extend( '$text', { allowAttributes: FONT_SIZE } );
-		editor.model.schema.setAttributeProperties( FONT_SIZE, {
-			isFormatting: true,
-			copyOnEnter: true
-		} );
+    // Allow fontSize attribute on text nodes.
+    editor.model.schema.extend("$text", { allowAttributes: FONT_SIZE });
+    editor.model.schema.setAttributeProperties(FONT_SIZE, {
+      isFormatting: true,
+      copyOnEnter: true
+    });
 
-		const supportAllValues = editor.config.get( 'fontSize.supportAllValues' );
+    const supportAllValues = editor.config.get("fontSize.supportAllValues");
 
-		// Define view to model conversion.
-		const options = normalizeOptions( this.editor.config.get( 'fontSize.options' ) )
-			.filter( item => item.model );
-		const definition = buildDefinition( FONT_SIZE, options );
+    // Define view to model conversion.
+    const options = normalizeOptions(
+      this.editor.config.get("fontSize.options")
+    ).filter(item => item.model);
+    const definition = buildDefinition(FONT_SIZE, options);
 
-		// Set-up the two-way conversion.
-		if ( supportAllValues ) {
-			this._prepareAnyValueConverters( definition );
-			this._prepareCompatibilityConverter();
-		} else {
-			editor.conversion.attributeToElement( definition );
-		}
+    // Set-up the two-way conversion.
+    if (supportAllValues) {
+      this._prepareAnyValueConverters(definition);
+      this._prepareCompatibilityConverter();
+    } else {
+      editor.conversion.attributeToElement(definition);
+    }
 
-		// Add FontSize command.
-		editor.commands.add( FONT_SIZE, new FontSizeCommand( editor ) );
-	}
+    // Add FontSize command.
+    editor.commands.add(FONT_SIZE, new FontSizeCommand(editor));
+  }
 
-	/**
-	 * These converters enable keeping any value found as `style="font-size: *"` as a value of an attribute on a text even
-	 * if it is not defined in the plugin configuration.
-	 *
-	 * @param {Object} definition {@link module:engine/conversion/conversion~ConverterDefinition Converter definition} out of input data.
-	 * @private
-	 */
-	_prepareAnyValueConverters( definition ) {
-		const editor = this.editor;
+  /**
+   * These converters enable keeping any value found as `style="font-size: *"` as a value of an attribute on a text even
+   * if it is not defined in the plugin configuration.
+   *
+   * @param {Object} definition {@link module:engine/conversion/conversion~ConverterDefinition Converter definition} out of input data.
+   * @private
+   */
+  _prepareAnyValueConverters(definition) {
+    const editor = this.editor;
 
-		// If `fontSize.supportAllValues=true`, we do not allow to use named presets in the plugin's configuration.
-		const presets = definition.model.values.filter( value => {
-			return !isLength( String( value ) ) && !isPercentage( String( value ) );
-		} );
+    // If `fontSize.supportAllValues=true`, we do not allow to use named presets in the plugin's configuration.
+    const presets = definition.model.values.filter(value => {
+      return !isLength(String(value)) && !isPercentage(String(value));
+    });
 
-		if ( presets.length ) {
-			/**
-			 * If {@link module:font/fontsize~FontSizeConfig#supportAllValues `config.fontSize.supportAllValues`} is `true`,
-			 * you need to use numerical values as font size options.
-			 *
-			 * See valid examples described in the {@link module:font/fontsize~FontSizeConfig#options plugin configuration}.
-			 *
-			 * @error font-size-invalid-use-of-named-presets
-			 * @param {Array.<String>} presets Invalid values.
-			 */
-			throw new CKEditorError(
-				'font-size-invalid-use-of-named-presets',
-				null, { presets }
-			);
-		}
+    if (presets.length) {
+      /**
+       * If {@link module:font/fontsize~FontSizeConfig#supportAllValues `config.fontSize.supportAllValues`} is `true`,
+       * you need to use numerical values as font size options.
+       *
+       * See valid examples described in the {@link module:font/fontsize~FontSizeConfig#options plugin configuration}.
+       *
+       * @error font-size-invalid-use-of-named-presets
+       * @param {Array.<String>} presets Invalid values.
+       */
+      throw new CKEditorError("font-size-invalid-use-of-named-presets", null, {
+        presets
+      });
+    }
 
-		editor.conversion.for( 'downcast' ).attributeToElement( {
-			model: FONT_SIZE,
-			view: ( attributeValue, { writer } ) => {
-				if ( !attributeValue ) {
-					return;
-				}
+    editor.conversion.for("downcast").attributeToElement({
+      model: FONT_SIZE,
+      view: (attributeValue, { writer }) => {
+        if (!attributeValue) {
+          return;
+        }
 
-				return writer.createAttributeElement( 'span', { style: 'font-size:' + attributeValue }, { priority: 7 } );
-			}
-		} );
+        return writer.createAttributeElement(
+          "span",
+          { style: "font-size:" + attributeValue },
+          { priority: 7 }
+        );
+      }
+    });
 
-		editor.conversion.for( 'upcast' ).elementToAttribute( {
-			model: {
-				key: FONT_SIZE,
-				value: viewElement => viewElement.getStyle( 'font-size' )
-			},
-			view: {
-				name: 'span',
-				styles: {
-					'font-size': /.*/
-				}
-			}
-		} );
-	}
+    editor.conversion.for("upcast").elementToAttribute({
+      model: {
+        key: FONT_SIZE,
+        value: viewElement => viewElement.getStyle("font-size")
+      },
+      view: {
+        name: "span",
+        styles: {
+          "font-size": /.*/
+        }
+      }
+    });
+  }
 
-	/**
-	 * Adds support for legacy `<font size="..">` formatting.
-	 *
-	 * @private
-	 */
-	_prepareCompatibilityConverter() {
-		const editor = this.editor;
+  /**
+   * Adds support for legacy `<font size="..">` formatting.
+   *
+   * @private
+   */
+  _prepareCompatibilityConverter() {
+    const editor = this.editor;
 
-		editor.conversion.for( 'upcast' ).elementToAttribute( {
-			view: {
-				name: 'font',
-				attributes: {
-					// Documentation mentions sizes from 1 to 7. To handle old content we support all values
-					// up to 999 but clamp it to the valid range. Why 999? It should cover accidental values
-					// similar to percentage, e.g. 100%, 200% which could be the usual mistake for font size.
-					'size': /^[+-]?\d{1,3}$/
-				}
-			},
-			model: {
-				key: FONT_SIZE,
-				value: viewElement => {
-					const value = viewElement.getAttribute( 'size' );
-					const isRelative = value[ 0 ] === '-' || value[ 0 ] === '+';
+    editor.conversion.for("upcast").elementToAttribute({
+      view: {
+        name: "font",
+        attributes: {
+          // Documentation mentions sizes from 1 to 7. To handle old content we support all values
+          // up to 999 but clamp it to the valid range. Why 999? It should cover accidental values
+          // similar to percentage, e.g. 100%, 200% which could be the usual mistake for font size.
+          size: /^[+-]?\d{1,3}$/
+        }
+      },
+      model: {
+        key: FONT_SIZE,
+        value: viewElement => {
+          const value = viewElement.getAttribute("size");
+          const isRelative = value[0] === "-" || value[0] === "+";
 
-					let size = parseInt( value, 10 );
+          let size = parseInt(value, 10);
 
-					if ( isRelative ) {
-						// Add relative size (which can be negative) to the default size = 3.
-						size = 3 + size;
-					}
+          if (isRelative) {
+            // Add relative size (which can be negative) to the default size = 3.
+            size = 3 + size;
+          }
 
-					const maxSize = styleFontSize.length - 1;
-					const clampedSize = Math.min( Math.max( size, 0 ), maxSize );
+          const maxSize = styleFontSize.length - 1;
+          const clampedSize = Math.min(Math.max(size, 0), maxSize);
 
-					return styleFontSize[ clampedSize ];
-				}
-			}
-		} );
-	}
+          return styleFontSize[clampedSize];
+        }
+      }
+    });
+  }
 }
